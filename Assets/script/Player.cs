e6aa112419f5312d4d3d27e3ae411d8e6a713dd3
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-11-14 20:18:19 +0800

    1.4
    add anim

---
+++ Assets/script/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject end;
+    public Text ciontx;
+    public int cion;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Particle System Flh")
+        {
+            end.SetActive(true);
+            FindObjectOfType<KID.CharacterMovement2D>().enabled = false;
+        }
+        if(collision.tag == "cherry")
+        {
+            Destroy(collision.gameObject);
+
+            cion++;
+            ciontx.text = cion + "/999";
+        }
+
+    }
+}
